b14c66f33809a7c69d9949703225464214d77b75
João Vitor Borato TARUMA <joaov.taruma@gmail.com>
2025-06-20 10:48:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def painel():
                                'Devolucao_Porcentagem': media_devolucao_porcentagem,
                                'Dispersao_KM': media_dispersao_km,
                                'Rating': media_rating,
-                               'Reposicao_Valor': f"{soma_reposicao:.2f}".replace('.', ','),
+                               'Reposicao_Valor': f"{soma_reposicao:.2f}".replace(',', '.'),
                                'Refugo_Porcentagem': media_refugo
                            },
                            prontuario=prontuario,
